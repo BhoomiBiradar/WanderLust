9e3071c299075d0c9cd161da044b04ddadab0da6
BhoomiBiradar <rbhoomi319@gmail.com>
2024-08-02 18:19:55 +0530

    Phase 2 part E

--- init/index.js
+++ init/index.js
@@ -18,8 +18,9 @@ async function main(){
 
 const initDB = async () =>{
     await Listing.deleteMany({});
+    initData.data =  initData.data.map((obj)=>({...obj,owner:'66ac75d603b03a27c6a936ca',}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
-}
+};
 
 initDB();
